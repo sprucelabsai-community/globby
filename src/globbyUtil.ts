422a6f57ce4aa172c894dfbd1e7d6f958a8b5ae9
Taylor Romero <me@taylorrome.ro>
2024-04-15 07:04:51 -0600

    major: eslint upgrade

--- src/globbyUtil.ts
+++ src/globbyUtil.ts
@@ -3,24 +3,24 @@ import { default as globbyNamespace } from 'globby'
 import coreGlobby from 'globby'
 
 const globby = async function globby(
-	patterns: string | string[],
-	options?: Partial<globbyNamespace.GlobbyOptions>
+    patterns: string | string[],
+    options?: Partial<globbyNamespace.GlobbyOptions>
 ) {
-	const pats = normalizePaths(patterns)
-	return coreGlobby(pats, options) as unknown as Promise<string[]>
+    const pats = normalizePaths(patterns)
+    return coreGlobby(pats, options) as unknown as Promise<string[]>
 }
 globby.sep = path.sep
 globby.sync = function (
-	patterns: string | string[],
-	options?: Partial<globbyNamespace.GlobbyOptions>
+    patterns: string | string[],
+    options?: Partial<globbyNamespace.GlobbyOptions>
 ) {
-	const pats = normalizePaths(patterns)
-	return coreGlobby.sync(pats, options)
+    const pats = normalizePaths(patterns)
+    return coreGlobby.sync(pats, options)
 }
 function normalizePaths(patterns: string | string[]) {
-	const normalized = Array.isArray(patterns) ? patterns : [patterns]
-	const pats = normalized.map((n) => n.split(globby.sep).join(path.posix.sep))
-	return pats
+    const normalized = Array.isArray(patterns) ? patterns : [patterns]
+    const pats = normalized.map((n) => n.split(globby.sep).join(path.posix.sep))
+    return pats
 }
 
 export default globby
